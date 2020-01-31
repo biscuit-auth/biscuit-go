3f59bb4b7d3fb61c237a48fdaf1ea9244ce10b18
Jonathan Rudenberg <jonathan@titanous.com>
2020-01-30 20:25:44 -0500

    wip

--- datalog/datalog.go
+++ datalog/datalog.go
@@ -1,6 +1,7 @@
 package datalog
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -35,7 +36,7 @@ type Integer int64
 
 func (Integer) Type() IDType { return IDTypeInteger }
 
-type String String
+type String string
 
 func (String) Type() IDType { return IDTypeString }
 
@@ -115,9 +116,9 @@ func (m *StringComparisonMatcher) Match(id ID) bool {
 	case StringComparisonEqual:
 		return m.String == v
 	case StringComparisonPrefix:
-		return strings.HasPrefix(v, string(m.String))
+		return strings.HasPrefix(string(v), string(m.String))
 	case StringComparisonSuffix:
-		return strings.HasSuffix(v, string(m.String))
+		return strings.HasSuffix(string(v), string(m.String))
 	default:
 		return false
 	}
@@ -210,13 +211,46 @@ func (p Predicate) Equal(p2 Predicate) bool {
 	return true
 }
 
+func (p Predicate) Match(p2 Predicate) bool {
+	if p.Name != p2.Name || len(p.IDs) != len(p2.IDs) {
+		return false
+	}
+	for i, id := range p.IDs {
+		_, v1 := id.(Variable)
+		_, v2 := p2.IDs[i].(Variable)
+		if v1 || v2 {
+			continue
+		}
+		if id != p2.IDs[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (p Predicate) Clone() Predicate {
+	res := Predicate{Name: p.Name, IDs: make([]ID, len(p.IDs))}
+	copy(res.IDs, p.IDs)
+	return res
+}
+
 type Fact struct {
 	Predicate
 }
 
 type Constraint struct {
-	ID uint32
+	Name Variable
 	Matcher
+}
+
+func (c Constraint) Check(name Variable, id ID) bool {
+	if c.Name != name {
+		return true
+	}
+	if _, ok := id.(Variable); ok {
+		panic("should not check constraint on a variable")
+	}
+	return c.Match(id)
 }
 
 type Rule struct {
@@ -225,37 +259,75 @@ type Rule struct {
 	Constraints []Constraint
 }
 
-func (r Rule) Apply(facts []Fact) ([]Fact, error) {
-	vars := make([]Variable, 0, len(r.Body))
+type InvalidRuleError struct {
+	Rule            Rule
+	MissingVariable Variable
+}
+
+func (e InvalidRuleError) Error() string {
+	return fmt.Sprintf("datalog: variable %d in head is missing from body and/or constraints")
+}
+
+func (r Rule) Apply(facts *FactSet, newFacts *FactSet) error {
+	variables := make(MatchedVariables)
 	for _, p := range r.Body {
 		for _, id := range p.IDs {
 			v, ok := id.(Variable)
 			if !ok {
 				continue
 			}
-			vars = append(vars, v)
+			variables[v] = nil
 		}
 	}
 
-	return nil, nil
+	for _, h := range NewCombinator(variables, r.Body, r.Constraints, facts).Combine() {
+		p := r.Head.Clone()
+		for i, id := range p.IDs {
+			k, ok := id.(Variable)
+			if !ok {
+				continue
+			}
+			v, ok := h[k]
+			if !ok {
+				return InvalidRuleError{r, k}
+			}
+			p.IDs[i] = *v
+		}
+		newFacts.Insert(Fact{p})
+	}
+
+	return nil
 }
 
 type Caveat struct {
 	Queries []Rule
 }
 
+type FactSet []Fact
+
+func (s *FactSet) Insert(f Fact) bool {
+	for _, v := range *s {
+		if v.Equal(f.Predicate) {
+			return false
+		}
+	}
+	*s = append(*s, f)
+	return true
+}
+
+func (s *FactSet) InsertAll(facts []Fact) {
+	for _, f := range facts {
+		s.Insert(f)
+	}
+}
+
 type World struct {
-	facts []Fact
+	facts *FactSet
 	rules []Rule
 }
 
 func (w *World) AddFact(f Fact) {
-	for _, v := range w.facts {
-		if v.Equal(f.Predicate) {
-			return
-		}
-	}
-	w.facts = append(w.facts, f)
+	w.facts.Insert(f)
 }
 
 func (w *World) AddRule(r Rule) {
@@ -263,13 +335,169 @@ func (w *World) AddRule(r Rule) {
 }
 
 func (w *World) Run() error {
-	return nil
+	for i := 0; i < 100; i++ {
+		var newFacts FactSet
+		for _, r := range w.rules {
+			if err := r.Apply(r.facts, &newFacts); err != nil {
+				return err
+			}
+		}
+		l := len(newFacts)
+		w.facts.InsertAll([]Fact(newFacts))
+		if len(w.facts) == l {
+			return nil
+		}
+	}
+	return fmt.Errorf("datalog: world ran more than 100 iterations")
 }
 
-func (w *World) Query(pred Predicate) []Fact {
-	return nil
+func (w *World) Query(pred Predicate) *FactSet {
+	res := &FactSet{}
+	for _, f := range *w.facts {
+		if f.Predicate.Name != pred.Name {
+			continue
+		}
+		minLen := len(f.Predicate.IDs)
+		if l := len(pred.IDs); l < minLen {
+			minLen = l
+		}
+		for i := 0; i < minLen; i++ {
+			fID := f.Predicate.IDs[i]
+			pID := pred.IDs[i]
+			if fID.Type() != IDTypeVariable && fid.Type() == pid.Type() {
+				if fID != pID {
+					continue
+				}
+			} else if fID.Type() != IDTypeSymbol && pID.Type() != IDTypeVariable {
+				continue
+			}
+			*res = append(*res, f)
+		}
+	}
+	return res
 }
 
-func (w *World) QueryRule(rule Rule) []Fact {
-	return nil
+func (w *World) QueryRule(rule Rule) *FactSet {
+	newFacts := &FactSet{}
+	rule.Apply(w.facts, newFacts)
+	return newFacts
+}
+
+type MatchedVariables map[Variable]*ID
+
+func NewMatchedVariables(vs map[Variable]struct{}) MatchedVariables {
+	res := make(MatchedVariables, len(vs))
+	for k := range vs {
+		res[k] = nil
+	}
+	return res
+}
+
+func (m MatchedVariables) Insert(k Variable, v ID) bool {
+	existing := m[k]
+	if existing == nil {
+		m[k] = &v
+		return true
+	}
+	return *existing == v
+}
+
+func (m MatchedVariables) Complete() map[Variable]*ID {
+	for _, v := range m {
+		if v == nil {
+			return nil
+		}
+	}
+	return (map[Variable]*ID)(m)
+}
+
+func (m MatchedVariables) Clone() MatchedVariables {
+	res := make(MatchedVariables, len(m))
+	for k, v := range m {
+		res[k] = v
+	}
+	return res
+}
+
+type Combinator struct {
+	variables    MatchedVariables
+	predicates   []Predicate
+	constraints  []Constraint
+	allFacts     *FactSet
+	currentFacts *FactSet
+}
+
+func NewCombinator(variables MatchedVariables, predicates []Predicate, constraints []Constraint, allFacts *FactSet) *Combinator {
+	c := &Combinator{
+		variables:   variables,
+		predicates:  predicates,
+		constraints: constraints,
+		allFacts:    allFacts,
+	}
+	currentFacts := make(FactSet, 0, len(*allFacts))
+	for _, f := range *allFacts {
+		if f.Match(predicates[0]) {
+			currentFacts = append(currentFacts, f)
+		}
+	}
+	c.currentFacts = &currentFacts
+	return c
+}
+
+func (c *Combinator) Combine() []map[Variable]*ID {
+	var variables []map[Variable]*ID
+	if len(c.predicates) == 0 {
+		if vars := c.variables.Complete(); vars != nil {
+			variables = append(variables, vars)
+		}
+		return variables
+	}
+	if len(*c.currentFacts) == 0 {
+		return variables
+	}
+
+	for i, pred := range c.predicates {
+		for _, currentFact := range *c.currentFacts {
+			vars := c.variables.Clone()
+			matchIDs := true
+			minLen := len(pred.IDs)
+			if l := len(currentFact.Predicate.IDs); l < minLen {
+				minLen = l
+			}
+
+			for j := 0; j < minLen; j++ {
+				id := pred.IDs[j]
+				k, ok := id.(Variable)
+				if !ok {
+					continue
+				}
+				v := currentFact.Predicate.IDs[j]
+				for _, con := range c.constraints {
+					if !con.Check(k, v) {
+						matchIDs = false
+						break
+					}
+				}
+				if !vars.Insert(k, v) {
+					matchIDs = false
+				}
+				if !matchIDs {
+					break
+				}
+			}
+
+			if !matchIDs {
+				continue
+			}
+
+			if len(c.predicates) > i+1 {
+				variables = append(variables, NewCombinator(vars, c.predicates[i+1:], c.constraints, c.allFacts).Combine()...)
+			} else {
+				if v := vars.Complete(); v != nil {
+					variables = append(variables, v)
+				}
+			}
+		}
+	}
+	return variables
 }
