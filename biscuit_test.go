430ab4d56af2aeda3de47877a0201e3fcc2dd38e
Flavien Binet <flavien.binet@gmail.com>
2020-08-21 17:04:58 +0200

    debug

--- biscuit_test.go
+++ biscuit_test.go
@@ -2,6 +2,7 @@ package biscuit
 
 import (
 	"crypto/rand"
+	"fmt"
 	"testing"
 
 	"github.com/flynn/biscuit-go/sig"
@@ -98,4 +99,75 @@ func TestBiscuit(t *testing.T) {
 	v3.AddOperation("write")
 	v3.AddResource("/a/file1")
 	require.Error(t, v3.Verify())
+}
+
+func TestBiscuitRules(t *testing.T) {
+	rng := rand.Reader
+	root := sig.GenerateKeypair(rng)
+
+	builder := NewBuilder(rng, root)
+
+	builder.AddAuthorityRule(Rule{
+		Head: Predicate{Name: "right", IDs: []Atom{Variable(1), Symbol("read")}},
+		Body: []Predicate{
+			{Name: "resource", IDs: []Atom{Symbol("ambient"), Variable(1)}},
+			{Name: "owner", IDs: []Atom{Symbol("ambient"), Variable(0), Variable(1)}},
+		},
+	})
+	builder.AddAuthorityRule(Rule{
+		Head: Predicate{Name: "right", IDs: []Atom{Variable(1), Symbol("write")}},
+		Body: []Predicate{
+			{Name: "resource", IDs: []Atom{Symbol("ambient"), Variable(1)}},
+			{Name: "owner", IDs: []Atom{Symbol("ambient"), Variable(0), Variable(1)}},
+		},
+	})
+
+	b1, err := builder.Build()
+	require.NoError(t, err)
+
+	block := b1.CreateBlock()
+	block.AddCaveat(Caveat{
+		Queries: []Rule{
+			{
+				Head: Predicate{Name: "caveat1", IDs: []Atom{Variable(0), Variable(1)}},
+				Body: []Predicate{
+					{Name: "right", IDs: []Atom{Symbol("authority"), Variable(0), Variable(1)}},
+					{Name: "resource", IDs: []Atom{Symbol("ambient"), Variable(0)}},
+					{Name: "operation", IDs: []Atom{Symbol("ambient"), Variable(1)}},
+				},
+			},
+			{
+				Head: Predicate{Name: "caveat2", IDs: []Atom{Variable(0)}},
+				Body: []Predicate{
+					{Name: "resource", IDs: []Atom{Symbol("ambient"), Variable(0)}},
+					{Name: "owner", IDs: []Atom{Symbol("ambient"), Symbol("alice"), Variable(0)}},
+				},
+			},
+		},
+	})
+
+	b2, err := b1.Append(rng, sig.GenerateKeypair(rng), block.Build())
+	require.NoError(t, err)
+
+	v, err := b2.Verify(root.Public())
+	require.NoError(t, err)
+
+	v.AddOperation("write")
+	v.AddResource("file1")
+	v.AddFact(Fact{
+		Predicate: Predicate{
+			Name: "owner",
+			IDs: []Atom{
+				Symbol("ambient"),
+				Symbol("alice"),
+				String("file1"),
+			},
+		},
+	})
+
+	fmt.Println(v.PrintWorld())
+	fmt.Println(b2.String())
+
+	require.NoError(t, v.Verify())
+	t.Fail()
 }
